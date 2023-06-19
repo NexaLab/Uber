11530e46fa22bb17e5593096da542abbf5e7a138
Afshal Hassan <afshal.hassan@venturedive.com>
2023-06-19 11:17:50 +0500

    Google Maps added

--- src/hooks/useLocationPermission/useLocationPermission.jsx
+++ src/hooks/useLocationPermission/useLocationPermission.jsx
@@ -19,40 +19,11 @@ function useLocationPermission() {
 
 
 
-    const getCurrentLocation = async () => {
-        try {
-            // const { status } = await Geolocation.requestAuthorization('whenInUse');
-            // if (status !== 'granted') {
-            //     console.log('Permission to access location was denied');
-            //     return;
-            // }
-
-            navigator.geolocation.getCurrentPosition(
-                position => {
-                    const { latitude, longitude } = position.coords;
-                    setCurrentLocation({ ...currentLocation, latitude, longitude });
-                },
-                error => {
-                    console.log('Error getting current location:', error);
-                }
-            );
-        } catch (error) {
-            console.log('Error getting current location:', error);
-        }
-    };
-
-
-
-
-
-
-
 
     const requestLocationPermission = async () => {
 
         let { status } = await Location.requestForegroundPermissionsAsync();
         if (status !== 'granted') {
-            setErrorMsg('Permission to access location was denied');
             return;
         }
 
